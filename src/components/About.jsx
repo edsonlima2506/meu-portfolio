fb074205709404180bd049084c206be88198810b
Edson Lima <edsoneduardo526@gmail.com>
2021-11-11 01:31:13 -0300

    Adiciona seçao de resumo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,22 @@ class About extends React.Component {
     render() {
         return (
             <div className="about" id="about">
-
+                <section className="resume">
+                    <div className="resumeContent">
+                        <h1>+2MIL</h1>
+                        <h2>Pessoas impactadas</h2>
+                    </div>
+                    <hr />
+                    <div className="resumeContent">
+                        <h1>20</h1>
+                        <h2>Projetos</h2>
+                    </div>
+                    <hr />
+                    <div className="resumeContent">
+                        <h1>CEO</h1>
+                        <h2>Aos 18 anos</h2>
+                    </div>
+                </section>
             </div>
         )
     }
