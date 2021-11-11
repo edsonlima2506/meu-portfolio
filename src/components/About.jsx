7efa1fff7b352dc5a0b49c63b29d60327f16b097
Edson Lima <edsoneduardo526@gmail.com>
2021-11-11 01:06:22 -0300

    Cria componente sobre

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../components/about.css';
+
+class About extends React.Component {
+    render() {
+        return (
+            <div className="about" id="about">
+
+            </div>
+        )
+    }
+}
+
+export default About;
