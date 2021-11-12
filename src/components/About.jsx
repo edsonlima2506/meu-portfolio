ef2c69b3dea0871fdf5e46b7e49c07378da391cd
Edson Lima <edsoneduardo526@gmail.com>
2021-11-12 15:41:04 -0300

    Adiciona conteudo de apresentaçao na seçao about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -24,7 +24,15 @@ class About extends React.Component {
                 </section>
                 <section className="myBio">
                     <img src={ fotoSobre } alt="foto de Edson Lima"/>
-                    <p>Prazer, sou o Edson!</p>
+                    <article>
+                        <h3 className="greetings">Prazer, sou o Edson! <br /> Dev Front-End </h3>
+                        <p className="bioApresentation">
+                            Sou um programdor que busca sempre a excêlencia na resolução de problemas, <br /> 
+                            entendendo sempre que a tecnologia é um meio para isso, e não um fim! <br />
+                            Carrego comigo uma comunidade composta por mais de 2mil desenvolvedores <br />
+                            e comando uma startup que já impactou mais de 10mil pessoas!
+                        </p>
+                    </article>
                 </section>
             </div>
         )
